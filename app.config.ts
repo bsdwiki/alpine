9b3be10fe86c91c11826fd4440527fe2f4a6c28b
bsdwiki <153282803+bsdwiki@users.noreply.github.com>
2023-12-12 00:28:49 +0800

    misc

--- app.config.ts
+++ app.config.ts
@@ -29,7 +29,7 @@ export default defineAppConfig({
       },
       navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me by below platforms' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
       twitter: '',
